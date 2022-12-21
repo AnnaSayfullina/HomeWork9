fc65c9249d37d6f362b2c4dc0f0664cc879de350
anna <anna@sayf.su>
2022-12-21 14:11:54 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    /** Бухгалтеры попросили посчитать сумму всех выплат за месяц.
+    * Напишите программу, которая решит эту задачу, и выведите в консоль результат в формате:
+    * «Сумма трат за месяц составила … рублей».
+    */
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element: arr) {
+            sum = sum + element;
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей \n", sum);
     }
 }
